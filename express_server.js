f0dddb3e57dea29d7f1113da5aaa59e6f08d4b9a
Kai Tang <16kt16@queensu.ca>
2017-10-11 20:45:30 +0000

    up to this step, Cookies in Express done

--- express_server.js
+++ express_server.js
@@ -1,11 +1,16 @@
 const express =require("express");
 const bodyParser =require("body-parser");
+const cookieParser =require("cookie-parser");
 const app = express();
+
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
+
+// let username="Not Registered Yet!";
 
 // let urlDatabase = {
 //   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,13 +22,17 @@ app.set("view engine", "ejs");
 
 app.get("/urls", (req, res)=>{
 
+  // console.log(req.cookies)
+
   let templateVars ={urls: urlDatabase,
-                    PORT: PORT};
+                    PORT: PORT,
+                    username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req,res)=>{
-  res.render("urls_new")
+  let templateVars ={username: req.cookies["username"]};
+  res.render("urls_new", templateVars)
 });
 
 app.post("/urls", (req, res)=>{
@@ -44,7 +53,8 @@ function generateRandomString(){
 app.get("/urls/:id", (req, res)=>{
   let templaterVars ={shortURL: req.params.id,
                       longURL: urlDatabase[req.params.id],
-                      link: `http://localhost:${PORT}/u/${req.params.id}`};
+                      link: `http://localhost:${PORT}/u/${req.params.id}`,
+                      username: req.cookies["username"]};
   res.render("urls_show", templaterVars);
 });
 
@@ -65,6 +75,19 @@ app.post("/urls/:id/updata", (req, res)=>{
   console.log(urlDatabase);
   res.redirect(`http://localhost:${PORT}/urls`);
 });
+
+app.post("/login", (req, res)=>{
+  let username=req.body.username;
+  console.log(username);
+  res.cookie("username", username);
+  res.redirect(`http://localhost:${PORT}/urls`);
+  });
+
+app.post("/logout", (req, res)=>{
+
+  res.clearCookie("username");
+  res.redirect(`http://localhost:${PORT}/urls`);
+  });
 
 
 let urlDatabase = {
