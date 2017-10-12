15f16c265db7fb6d34b5469c9ba9804a9e1ae77b
Kai Tang <16kt16@queensu.ca>
2017-10-12 19:53:46 +0000

    User Specific Features done

--- express_server.js
+++ express_server.js
@@ -14,31 +14,53 @@ app.use(cookieParser())
 app.set("view engine", "ejs");
 
 app.get("/", (req, res)=>{
-  res.redirect(`http://localhost:${PORT}/login`)
+  let templateVars ={urls: urlDatabase,
+                       PORT: PORT};
+  res.render("home", templateVars);
 });
 
 
 app.get("/urls", (req, res)=>{
-  let templateVars ={urls: urlDatabase,
-                     PORT: PORT};
-  res.render("urls_index", templateVars);
+  let ID =req.cookies["user_id"];
+  if(ID){
+    let templateVars ={urls: urlDatabase,
+                         ID:ID,
+                       PORT: PORT};
+    res.render("urls_index", templateVars);
+  }else{
+    let templateVars ={PORT: PORT,
+                 StatusCode:400};
+    res.render("login_reminder", templateVars);
+  }
 });
 
 
 app.get("/urls/new", (req,res)=>{
-  // currentID =req.cookies["user_id"]
-  let templateVars ={PORT:PORT};
-  res.render("urls_new", templateVars)
+
+  if(req.cookies["user_id"]){
+    let templateVars ={PORT:PORT};
+    res.render("urls_new", templateVars)
+  }else{
+    let templateVars ={PORT: PORT,
+                 StatusCode:400};
+    res.render("login_reminder", templateVars);
+  }
 });
 
 
 app.post("/urls", (req, res)=>{
-  let longUrl=req.body;
+  let ID =req.cookies["user_id"];
+  console.log(ID);
+  let longUrl=req.body.longURL;
   let shortUrl =generateRandomString();
-  urlDatabase[shortUrl]=longUrl.longURL;
-  console.log(urlDatabase);
+  if(!urlDatabase[ID]) {
+    urlDatabase[ID] = {[shortUrl]:longUrl};
+  }else{
+    urlDatabase[ID][shortUrl]= longUrl;
+  }
   res.redirect(`http://localhost:${PORT}/urls`);
 });
+
 
 
 function generateRandomString(){
@@ -50,28 +72,56 @@ function generateRandomString(){
 
 
 app.get("/urls/:id", (req, res)=>{
-  let templaterVars ={shortURL: req.params.id,
-                      longURL: urlDatabase[req.params.id],
+  let ID =req.cookies["user_id"];
+  let par = req.params.id;
+  let test =urlCheck(ID, par);
+
+  if(test){
+    let templaterVars ={shortURL: req.params.id,
+                      longURL: urlDatabase[ID][req.params.id],
                       PORT:PORT};
   res.render("urls_show", templaterVars);
+  }else{
+    let templateVars ={PORT: PORT,
+                 StatusCode:403};
+    res.render("login_reminder", templateVars);
+  }
 });
 
 
-app.get("/u/:shortURL", (req, res)=>{
-  let longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
-});
+function urlCheck(ID, par){
+
+  for(let key in urlDatabase){
+    if(key === ID){
+      for(let su in urlDatabase[ID]){
+        if(su===par){
+          return true;
+       }
+
+      }
+    }
+  }
+
+  return false;
+};
+
+// app.get("/u/:shortURL", (req, res)=>{
+//   let longURL = urlDatabase[req.params.shortURL]
+//   res.redirect(longURL);
+// });
 
 
 app.post("/urls/:id/delete", (req, res)=>{
-  delete urlDatabase[req.params.id]
+  let ID =req.cookies["user_id"];
+  delete urlDatabase[ID][req.params.id]
   res.redirect(`http://localhost:${PORT}/urls`);
 });
 
 
 app.post("/urls/:id/updata", (req, res)=>{
+  let ID =req.cookies["user_id"];
   let longUrl=req.body;
-  urlDatabase[req.params.id]=longUrl.longURL;
+  urlDatabase[ID][req.params.id]=longUrl.longURL;
   console.log(urlDatabase);
   res.redirect(`http://localhost:${PORT}/urls`);
 });
@@ -79,12 +129,15 @@ app.post("/urls/:id/updata", (req, res)=>{
 
 app.get("/login", (req, res)=>{
   let currentID = req.cookies["user_id"];
-  console.log(currentID);
+
   if(currentID){
+    console.log(users[currentID]);
+    console.log(users);
     let templateVars ={PORT:PORT,
                        user:users[currentID]};
     res.render("login", templateVars);
   }else{
+    console.log("0",currentID);
     let templateVars ={PORT:PORT,
                        user:""};
     res.render("login", templateVars);
@@ -131,6 +184,7 @@ app.post("/register", (req, res)=>{
               password:Pw};
     res.cookie("user_id", useID);
     users[useID]=newUer;
+    console.log(users);
     res.redirect(`http://localhost:${PORT}/urls`);
   }
   console.log(users);
@@ -157,9 +211,9 @@ function generateUserID(){
 
 
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-  };
+  "123":{"b2xVn2": "http://www.lighthouselabs.ca"},
+  "345":{"9sm5xK": "http://www.google.com"}
+   };
 
 
 const users = {
